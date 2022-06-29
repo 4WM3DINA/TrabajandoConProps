c3d856ce5e534af8d55463cbc5dd6f8f4c059329
Ambar Medina <ambmedina8@gmail.com>
2022-06-29 19:58:11 -0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <div id="app">
+    <h1>Listado de tareas</h1>
+    <hr />
+    <img
+      alt="Tareas"
+      width="300px"
+      height="300px"
+      src="../src/assets/TAREAS.jpg"
+    />
+    <ChildrenComponent
+      :tareas="tareas"
+      @enviarTarea="agregarTarea"
+      @quitarTarea="eliminarTarea"
+    />
+  </div>
+</template>
+
+<script>
+import ChildrenComponent from "./components/ChildrenComponent.vue";
+
+export default {
+  name: "App",
+  components: {
+    ChildrenComponent,
+  },
+  data() {
+    return {
+      tareas: [],
+    };
+  },
+  methods: {
+    agregarTarea(task) {
+      this.tareas.push(task);
+    },
+    eliminarTarea(i) {
+      this.tareas.splice(i, 1);
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #25257c;
+  font-size: 20px;
+  font-weight: bold;
+  background-image: url("https://img.freepik.com/vector-gratis/borde-disenos-florales-flores-rosadas_1305-5301.jpg?w=2000");
+  background-size: cover;
+  background-repeat: no-repeat;
+}
+</style>
---
+++ src/components/ChildrenComponent.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="hijo">
+    <div class="contenedor">
+      <form @submit.prevent="enviarTarea(tarea)">
+        Tarea
+        <input
+          type="text"
+          placeholder="ingresa nueva tarea"
+          v-model="tarea"
+          class="place"
+        />
+        <button type="submit" class="button">Añade</button>
+      </form>
+      <ul class="lista">
+        <li v-for="(valor, index) in tareas" :key="index">
+          {{ valor }}
+          <button @click.prevent="quitarTarea(index)">Eliminar</button>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ChildrenComponent",
+  props: {
+    tareas: {
+      type: Array,
+    },
+  },
+  data() {
+    return {
+      tarea: "",
+    };
+  },
+  methods: {
+    enviarTarea(tarea) {
+      if (tarea) {
+        this.$emit("enviarTarea", tarea);
+        this.tarea = "";
+      }
+    },
+    quitarTarea(i) {
+      this.$emit("quitarTarea", i);
+    },
+  },
+};
+</script>
+
+<style scoped>
+body {
+  margin: 0;
+  padding: 0;
+}
+.hijo {
+  text-align: center;
+}
+.lista {
+  text-align: left;
+  margin-left: 40vw;
+}
+.button {
+  background-color: rgb(243, 207, 45);
+  border: 3px solid rgb(8, 8, 8);
+  margin: 0.4rem;
+}
+.place {
+  background-color: rgb(233, 145, 214);
+  border: 3px solid rgb(8, 8, 8);
+  font-weight: bold;
+}
+</style>
